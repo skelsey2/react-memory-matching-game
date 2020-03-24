01f437c1882f3f33247f8d0bda4fa9b40bcd36fd
Steven Kelsey <skelsey212@gmail.com>
2020-03-23 20:44:38 -0400

    adding docker support

---
+++ memory-matching-api/src/main/java/com/example/todo/controller/TodoController.java
@@ -0,0 +1,19 @@
+package com.example.todo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TodoController {
+    @GetMapping
+    public String getTodo() {
+        return "A todo";
+    }
+
+    @GetMapping("mine")
+    public String getMyTodo() {
+        return "My todo";
+    }
+}
